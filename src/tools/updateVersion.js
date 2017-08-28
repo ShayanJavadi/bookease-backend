a83ae412614557a3b5985c2a938ef7adbd203524
Dewey Nguyen <dewey@rethinkworkflow.com>
2017-08-28 09:44:54 -0500

    update versionNumber

--- src/tools/updateVersion.js
+++ src/tools/updateVersion.js
@@ -3,6 +3,7 @@ import fs from "fs-extra";
 import extend from "lodash/extend";
 import path from "path";
 import pkg from "../../package.json";
+import L from "../logger/logger";
 
 const {buildNumber} = argv;
 
@@ -17,3 +18,5 @@ const newVersion = parts.join(".");
 fs.outputJsonSync(path.resolve(__dirname, "../../package.json"), extend({}, pkg, {version: newVersion}), {
   spaces: 2,
 });
+
+L.info(`Version has been updated ${newVersion}.`);
