f7c90b7ecc78f6783dd0a59becd3246d29b5e1db
Dewey Nguyen <dewey@rethinkworkflow.com>
2017-09-04 07:37:35 -0500

    change to bookease-workspace for firebase

--- src/libs/configureCors.js
+++ src/libs/configureCors.js
@@ -1,16 +1,17 @@
 import cors from "cors";
 import isEmpty from "lodash/isEmpty";
 import getVariable from "../config/getVariable";
+import L from "../logger/logger";
 
 export default (app) => {
   const whitelist = getVariable("CORS_ALLOWED_ORIGINS").split(",");
-
   const corsOptions = {
     credentials: true
   };
 
   if (!isEmpty(whitelist)) {
     corsOptions.origin = (origin, callback) => {
+      L.info(`origin: ${origin}, whitelist: ${whitelist.join(',')}`);
       if (whitelist.indexOf(origin) !== -1) {
         callback(null, true);
       } else {
