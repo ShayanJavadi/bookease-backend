563350f80120eddbd8f06b7ec6f70431322c7ca6
Dewey Nguyen <dewey@rethinkworkflow.com>
2017-09-07 12:25:22 -0500

    api for looking up books using google books API

---
+++ src/graphql/queries/lookupTextbooks.js
@@ -0,0 +1,49 @@
+import {GraphQLInt, GraphQLString} from "graphql";
+import TextbookLookupResult from "../types/TextbookLookupResult";
+import google from "googleapis";
+import B from "bluebird";
+import {get, map} from "lodash";
+import isISBN from "validator/lib/isISBN";
+
+export default {
+  type: TextbookLookupResult,
+  args: {
+    query: {
+      type: GraphQLString,
+      description: "query to search, provide isbn directly will search by isbn",
+    },
+    limit: {
+      type: GraphQLInt,
+    },
+    offset: {
+      type: GraphQLInt,
+    }
+  },
+  resolve: (req, {query, limit = 10, offset = 0}) => {
+    const books = google.books("v1");
+    const {volumes: {list}} = books;
+    const q = isISBN(query) ? `isbn:${query}` : query;
+
+    return B.promisify(list, books)({
+      q,
+      maxResults: limit,
+      startIndex: offset,
+      orderBy: "relevance"
+    })
+      .spread((response) => {
+        return {
+          totalItems: get(response, "totalItems", 0),
+          textbooks: map(get(response, "items", []), (item) => {
+            return {
+              id: get(item, "id"),
+              uid: get(item, "id"),
+              title: get(item, "volumeInfo.title"),
+              authors: get(item, "volumeInfo.authors", []),
+              industryIdentifiers: get(item, "volumeInfo.industryIdentifiers", []),
+              imageLinks: get(item, "volumeInfo.imageLinks", []),
+            };
+          })
+        };
+      });
+  },
+};
