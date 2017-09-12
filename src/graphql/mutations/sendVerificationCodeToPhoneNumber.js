f98844838c567ee7d2193368d254c6b355d704c2
Dewey Nguyen <dewey@rethinkworkflow.com>
2017-09-12 11:37:32 -0500

    sending verification code via phone number

---
+++ src/graphql/mutations/sendVerificationCodeToPhoneNumber.js
@@ -0,0 +1,44 @@
+import {GraphQLInt, GraphQLNonNull, GraphQLString} from "graphql";
+import isEmpty from "lodash/isEmpty";
+import db from "../../db";
+import generateRandomVerificationCode from "../../db/models/User/generateRandomVerificationCode";
+import sendVerificationCodeToPhoneNumber from "../../db/models/User/sendVerificationCodeToPhoneNumber";
+
+export default {
+  type: GraphQLInt,
+  args: {
+    phoneNumber: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  },
+  resolve: (req, args) => {
+    const {models: {User}} = db;
+    const {phoneNumber} = args;
+
+    return User.findOne({
+      where: {
+        phoneNumber,
+      },
+    })
+      .then((user) => {
+        if (isEmpty(user)) {
+          return User.build({
+            phoneNumber,
+            isVerified: false,
+          })
+            .save();
+        }
+
+        return user;
+      })
+      .then((user) => {
+        if (user.isVerified) {
+          throw new Error("The phoneNumber has already verified!");
+        }
+
+        return generateRandomVerificationCode(user);
+      })
+      .then(saved => sendVerificationCodeToPhoneNumber(saved))
+      .then(() => 200);
+  },
+};
