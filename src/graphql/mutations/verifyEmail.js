498b013e4b59d75d117f0bbd69fb27662b0627b5
Dewey Nguyen <dewey@rethinkworkflow.com>
2017-09-12 11:09:52 -0500

    verify email with code

---
+++ src/graphql/mutations/verifyEmail.js
@@ -0,0 +1,41 @@
+import {GraphQLInt, GraphQLNonNull, GraphQLString} from "graphql";
+import isEmpty from "lodash/isEmpty";
+import db from "../../db";
+
+export default {
+  type: GraphQLInt,
+  args: {
+    email: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+    verificationCode: {
+      type: new GraphQLNonNull(GraphQLString),
+    },
+  },
+  resolve: (req, args) => {
+    const {models: {User}} = db;
+    const {email, verificationCode} = args;
+
+    return User.findOne({
+      where: {
+        email,
+        verificationCode
+      },
+    })
+      .then((user) => {
+        if (isEmpty(user)) {
+          throw new Error("The entered email and verification code does not match!", 400);
+        }
+
+        if (user.isVerified) {
+          throw new Error("The email has already verified!", 400);
+        }
+
+        return user.update({
+          isVerified: true,
+          verificationCode: null
+        });
+      })
+      .then(() => 200);
+  },
+};
