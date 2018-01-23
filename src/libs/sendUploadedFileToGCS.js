b0ea424e1805a33253f987deda3021b851e4682a
shayanjavadi <shayanjavadi1375@gmail.com>
2018-01-22 20:53:02 -0600

    added thumbnail creation for progressive image loading

--- src/libs/sendUploadedFileToGCS.js
+++ src/libs/sendUploadedFileToGCS.js
@@ -1,5 +1,6 @@
 import isEmpty from "lodash/isEmpty";
 import GoogleCloudStorage from "@google-cloud/storage";
+import jimp from "jimp";
 import getVariable from "../config/getVariable";
 
 export default (req, res, next) => {
@@ -7,7 +8,6 @@ export default (req, res, next) => {
     return res.status(400)
       .send("No files were uploaded!");
   }
-
   const serviceAccount = {
     type: "service_account",
     project_id: getVariable("FIREBASE_PROJECT_ID"),
@@ -28,7 +28,8 @@ export default (req, res, next) => {
 
   const bucketName = getVariable("FIREBASE_STORAGE_BUCKET");
   const bucket = gcs.bucket(bucketName);
-  const fileNameInGCS = `${Date.now().valueOf()}-${req.file.originalname}`;
+  const dateIdentifier = Date.now().valueOf();
+  const fileNameInGCS = `${dateIdentifier}-${req.file.originalname}`;
   const file = bucket.file(fileNameInGCS);
 
   const stream = file.createWriteStream({
@@ -36,6 +37,19 @@ export default (req, res, next) => {
       contentType: req.file.mimetype,
     },
   });
+
+  const createThumbnail = (buffer, callback) => {
+    jimp.read(buffer, (readError, image) => {
+      const thumbnail = image.resize(10, 10);
+      thumbnail.getBuffer("image/png", (bufferError, base64) => {
+        const base64File = bucket.file(`${fileNameInGCS.replace(/(\.[\w\d_-]+)$/i, "-thumbnail$1")}`);
+        base64File.save(base64, {
+          public: true,
+          validation: "md5",
+        }).then(callback);
+      });
+    });
+  };
 
   stream.on("error", (err) => {
     req.file.cloudStorageError = err; // eslint-disable-line
@@ -45,8 +59,10 @@ export default (req, res, next) => {
   stream.on("finish", () => {
     req.file.cloudStorageObject = fileNameInGCS; // eslint-disable-line
     file.makePublic().then(() => {
-      req.file.cloudStoragePublicUrl = `https://storage.googleapis.com/${bucketName}/${fileNameInGCS}`; // eslint-disable-line
-      next();
+      createThumbnail(req.file.buffer, () => {
+        req.file.cloudStoragePublicUrl = `https://storage.googleapis.com/${bucketName}/${fileNameInGCS}`; // eslint-disable-line
+        next();
+      });
     });
   });
 
