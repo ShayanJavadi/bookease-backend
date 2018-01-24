5f68b4b0845cde27b44e9505ab7b3b0555197dad
shayan <shayanjavadi1375@gmail.com>
2018-01-24 03:43:28 +0000

    sendUploadedFileToGCS.js edited online with Bitbucket

--- src/libs/sendUploadedFileToGCS.js
+++ src/libs/sendUploadedFileToGCS.js
@@ -48,7 +48,7 @@ export default (req, res, next) => {
           validation: "md5",
         }, (err) => {
           if (err) {
-            return reject();
+            return reject(err);
           }
 
           return resolve();
