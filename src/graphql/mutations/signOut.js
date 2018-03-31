73fee53fc1a380de4db1427ae3aebc0ba3488701
shayanjavadi <shayanjavadi1375@gmail.com>
2018-03-30 20:31:06 -0500

    added schoolId to textbook. limited users to their schools.

--- src/graphql/mutations/signOut.js
+++ src/graphql/mutations/signOut.js
@@ -5,6 +5,8 @@ export default {
   resolve: (req) => {
     const {session} = req;
     session.userId = null; // eslint-disable-line
+    session.schoolId = null; // eslint-disable-line
+    session.pushNotificationToken = null; // eslint-disable-line
     return 200;
   },
 };
